a25ebd8f0e50212be4aa898d7b8509d0b2a1f8d0
Guru Sarath <gurusarath1.official@gmail.com>
2023-12-24 20:12:06 -0500

    Update main.py
    error fix

--- video_recording_open_cv/main.py
+++ video_recording_open_cv/main.py
@@ -2,7 +2,7 @@ import cv2
 
 if __name__ == '__main__':
 
-    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW))
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     while cap.isOpened():
         check, frame = cap.read()
 
